a238191238eab01ef2845c40d1674740968359fc
Eagle Academy <naviaulakh26@gmail.com>
2026-02-15 01:08:49 +0000

    Update server.ts

--- garagealert-FINAL/src/lib/supabase/server.ts
+++ garagealert-FINAL/src/lib/supabase/server.ts
@@ -1,8 +1,8 @@
 import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
-export async function createServerSupabaseClient() {
-  const cookieStore = await cookies()
+export async function createClient() {
+  const cookieStore = cookies()
 
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -18,7 +18,6 @@ export async function createServerSupabaseClient() {
               cookieStore.set(name, value, options)
             )
           } catch {
-            // This can be ignored if called from a Server Component
           }
         },
       },
